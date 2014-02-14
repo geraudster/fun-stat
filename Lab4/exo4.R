093f8fa085f1db83d6ef42b0fb0475976bf8866a
Geraud <geraudster@gmail.com>
2014-02-14 18:20:03 +0100

    suite exo3

--- Lab4/exo4.R
+++ Lab4/exo4.R
@@ -1,3 +1,18 @@
+library(reshape2)
+
+# Données initiales
 A <- c(8,9,4,8,8, NA)
 B <- c(7,2,5,7,5,3)
+
+# Merge et tri des donnees
 data <- data.frame(A,B)
+merge <- melt(data, na.rm=T)
+sorted <- merge[order(merge$value),]
+
+# Calcul des rangs
+sorted$rank <- rank(sorted$value)
+sorted
+
+# Totaux des rangs par groupe
+aggregate(sorted$rank, by=list(sorted$variable), FUN=sum)
+
