8ed1f6c41919d365b0e7ff4a7f7b90c1ccd8ae61
Geraud <geraudster@gmail.com>
2014-02-16 23:25:04 +0100

    Fin exo4:

--- Lab4/exo4.R
+++ Lab4/exo4.R
@@ -14,5 +14,32 @@ sorted$rank <- rank(sorted$value)
 sorted
 
 # Totaux des rangs par groupe
-aggregate(sorted$rank, by=list(sorted$variable), FUN=sum)
+agg <- aggregate(sorted$rank, by=list(sorted$variable), FUN=sum)
 
+nbA = length(na.omit(A))
+agg[1,2] - (nbA * (nbA + 1))/2
+
+nbB = length(na.omit(B))
+agg[2,2] - (nbB * (nbB + 1))/2
+
+wilcox.test(A,B)
+wilcox.test(B,A)
+t(sorted)
+
+wilcox.test(data[4,1], data[4,2], paired=T)
+
+setwd('/Documents/MOOC-stat/Lab1')
+alldata <- read.csv2('Geraud.csv', sep=';', header=T)
+wilcox.test(alldata$Var4, alldata$Var6, paired=T)
+
+wilcox.test(alldata$Var3, alldata$Var5, paired=T)
+
+
+require (ggplot2)
+long <- melt(alldata)
+
+ggplot(subset(long, variable == 'Var4' | variable == 'Var6'), aes (x=value, colour=variable)) +
+  geom_density()
+
+ggplot(subset(long, variable == 'Var3' | variable == 'Var5'), aes (x=value, colour=variable)) +
+  geom_density()
